84f14ae8935f7394fc4a718008c6533be1f81ee4
Faizan <faizan.sarwer@gmail.com>
2025-12-06 10:34:05 +0530

    Basic version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{
+    collections::{BTreeMap, HashMap},
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("weather_stations.csv").unwrap();
+    let f = BufReader::new(f);
+    let mut stats = HashMap::<String, (f64, f64, f64, usize)>::new();
+    for line in f.lines() {
+        let line = line.unwrap();
+        print!("{}", line);
+        let (station, temprature) = line.split_once(";").unwrap();
+        let temprature = temprature.parse().unwrap();
+        let stats = stats.entry(station.to_string()).or_default();
+        stats.0 = stats.0.min(temprature);
+        stats.1 = stats.1.max(temprature);
+
+        stats.2 += temprature;
+        stats.3 += 1;
+    }
+
+    print!("{{");
+    let stats = BTreeMap::from_iter(stats);
+    let mut stats = stats.into_iter().peekable();
+    while let Some((station, (min, max, total, count))) = stats.next() {
+        print!(
+            "{station}={min:.1}/{:.1}/{max:.1}",
+            total / { count as f64 }
+        );
+        if stats.peek().is_some() {
+            print!(", ")
+        }
+    }
+    print!("}}");
 }
